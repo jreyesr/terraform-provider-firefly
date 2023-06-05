87e8101aa8673c65c410bf957ea44e6c21601844
jreyesr <jreyesr@users.noreply.github.com>
2023-06-05 18:53:26 -0500

    Render docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,5 +1,5 @@
 provider "firefly" {
-  endpoint = "http://localhost:8080"
-  client_id = "1"
+  endpoint      = "http://localhost:8080"
+  client_id     = "1"
   client_secret = "UW/SQq/jOa35kg4WnUYaFoZMyN4SHzz4xHnf9rN4r5A="
 }
